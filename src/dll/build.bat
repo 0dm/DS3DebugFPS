1a4ae065722c39d2ab88a3deb18b54bf92c42966
Zofrea <johnmoreira1019@gmail.com>
2025-06-23 16:47:43 -0400

    Update src/dll/build.bat
    Co-authored-by: Aaron <57018940+0dm@users.noreply.github.com>

--- src/dll/build.bat
+++ src/dll/build.bat
@@ -1,2 +1,2 @@
-@gcc -o D3DCompiler_43.dll asm.s dllmain.c -O3 -lmsvcrt -lntdll -shared -Wno-multichar -Wl,--image-base,0x150000000,-e,DllMain
+@gcc -o D3DCompiler_43.dll asm.s dllmain.c -O3 -lmsvcrt -lntdll -lpsapi -nostartfiles -shared -Wno-multichar -Wl,--image-base,0x150000000,-e,DllMain
 @pause
